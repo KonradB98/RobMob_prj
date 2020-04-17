2860279e7a32fb29bef3db26da593d925d61c505
Anna Plociennik <aniap48@gmail.com>
2020-04-17 14:12:04 +0200

    Przetestowanie działania lewego czujnika

--- RoMo_prj/RoMo_prj.ino
+++ RoMo_prj/RoMo_prj.ino
@@ -1,4 +1,5 @@
 #define GRANICA 250 // poniżej białe, powyżej czarne
+#define LED 13
 
 const int left_sensor_pin =A0; //Ustawienie pinu analogowego podlaczonego do lewego czujnika
 int left_sensor_state; //Wartosc odczytu z lewego czujnika
@@ -7,6 +8,8 @@ const int right_sensor_pin =A1; // Analogicznie jak wyżej tylko tym razem dla p
 int right_sensor_state;
   
 void setup() {
+  pinMode(LED, OUTPUT); 
+ digitalWrite(LED, 0); //Wylaczenie diody
 Serial.begin(9600);//Otworzenie portu szeregowego do monitorowania danych z czujnika 
 }
 
@@ -15,10 +18,13 @@ void loop() {
  right_sensor_state = analogRead(right_sensor_pin);//Przypisanie wartosci sczytanej z prawego czujnika
  
  //Wypisanie komunikatu i wartosci odczytu do konsoli
-  Serial.print("Lewy czujnik: ");
+  if ( left_sensor_state > GRANICA) digitalWrite(LED, 1);
+  else digitalWrite(LED, 0); //jeśli czujnik widzi czarną linie to zapala diodę
+  
+ /* Serial.print("Lewy czujnik: ");
   Serial.println(left_sensor_state);
   delay(1000);
   Serial.print("Prawy czujnik: ");
   Serial.println(right_sensor_state);
-  delay(1000);
+  delay(1000);*/
 }
