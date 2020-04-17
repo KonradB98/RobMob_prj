9d4e9e3b735a88d22ad5c70e8b5b337009c51beb
Konrad Bajak <konrad.bajak@gmail.com>
2020-04-17 14:00:32 +0200

    Dodanie odczytu z drugiego czujnika oraz zdefiniowanie granicy odczytu

--- RoMo_prj.ino
+++ RoMo_prj.ino
@@ -1,5 +1,10 @@
-const int left_sensor_pin =A0; //Ustawienie pinu analogowego
+#define GRANICA 250 // poniżej białe, powyżej czarne
+
+const int left_sensor_pin =A0; //Ustawienie pinu analogowego podlaczonego do lewego czujnika
 int left_sensor_state; //Wartosc odczytu z lewego czujnika
+
+const int right_sensor_pin =A1; // Analogicznie jak wyżej tylko tym razem dla prawego czujnika
+int right_sensor_state;
   
 void setup() {
 Serial.begin(9600);//Otworzenie portu szeregowego do monitorowania danych z czujnika 
@@ -7,8 +12,13 @@ Serial.begin(9600);//Otworzenie portu szeregowego do monitorowania danych z czuj
 
 void loop() {
  left_sensor_state = analogRead(left_sensor_pin);//Przypisanie wartosci sczytanej z lewego czujnika
+ right_sensor_state = analogRead(right_sensor_pin);//Przypisanie wartosci sczytanej z prawego czujnika
+ 
  //Wypisanie komunikatu i wartosci odczytu do konsoli
   Serial.print("Lewy czujnik: ");
   Serial.println(left_sensor_state);
-  delay(250);
+  delay(1000);
+  Serial.print("Prawy czujnik: ");
+  Serial.println(right_sensor_state);
+  delay(1000);
 }
