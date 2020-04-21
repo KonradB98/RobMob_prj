a15719fedfe63f792f2d1e598cc77aa72795ae80
Konrad Bajak <konrad.bajak@gmail.com>
2020-04-21 22:11:54 +0200

    Dodanie silnikow i mostka H

--- RoMo_prj/RoMo_prj.ino
+++ RoMo_prj/RoMo_prj.ino
@@ -1,5 +1,13 @@
 #define GRANICA 250 // poniżej białe, powyżej czarne
 #define LED 13
+//Piny mostka H
+const int motorAspeed  = 5; 
+const int motorA1      = 6;  
+const int motorA2      = 7; 
+  
+const int motorB1      = 8; 
+const int motorB2      = 9; 
+const int motorBspeed  = 10; 
 
 const int left_sensor_pin =A0; //Ustawienie pinu analogowego podlaczonego do lewego czujnika
 int left_sensor_state; //Wartosc odczytu z lewego czujnika
@@ -8,23 +16,26 @@ const int right_sensor_pin =A1; // Analogicznie jak wyżej tylko tym razem dla p
 int right_sensor_state;
   
 void setup() {
-  pinMode(LED, OUTPUT); 
+ pinMode(motorA1, OUTPUT);
+ pinMode(motorA2, OUTPUT);
+ pinMode(motorB1, OUTPUT);
+ pinMode(motorB2, OUTPUT);
+ pinMode(LED, OUTPUT); 
  digitalWrite(LED, 0); //Wylaczenie diody
 Serial.begin(9600);//Otworzenie portu szeregowego do monitorowania danych z czujnika 
 }
 
 void loop() {
- left_sensor_state = analogRead(left_sensor_pin);//Przypisanie wartosci sczytanej z lewego czujnika
- right_sensor_state = analogRead(right_sensor_pin);//Przypisanie wartosci sczytanej z prawego czujnika
+
+  digitalWrite (motorA1,LOW);
+  digitalWrite(motorA2,HIGH);                       
+  digitalWrite (motorB1,LOW);
+  digitalWrite(motorB2,HIGH);
+  
+ left_sensor_state = analogRead(left_sensor_pin);
+ right_sensor_state = analogRead(right_sensor_pin);
  
-  if ( right_sensor_state > GRANICA) digitalWrite(LED, 1);
- else digitalWrite(LED, 0); //jeśli czujnik widzi czarną linie to zapala diodę
- 
-  //Wypisanie komunikatu i wartosci odczytu do konsoli
- /* Serial.print("Lewy czujnik: ");
-  Serial.println(left_sensor_state);
-  delay(1000);
-  Serial.print("Prawy czujnik: ");
-  Serial.println(right_sensor_state);
-  delay(1000);*/
+ if ( right_sensor_state > GRANICA) digitalWrite(LED, 1);
+ else digitalWrite(LED, 0); 
+
 }
