2561a9112cd75ec229049215423eb72ca9341d55
Konrad Bajak <konrad.bajak@gmail.com>
2020-06-04 20:19:12 +0200

    Implementacja algorytmu PID z uzyciem struktury

--- RoMo_prj/RoMo_prj.ino
+++ RoMo_prj/RoMo_prj.ino
@@ -41,6 +41,7 @@ struct pid_t{
 };
 
 struct pid_t pid;
+int pid_motors;
 
 //Nastawy regulatora
 const float Kp = 41;  
@@ -64,13 +65,20 @@ void setup() {
   pid.total_min = pid_scale(&pid, -255);
   
   Serial.begin(9600);//Otworzenie portu szeregowego do monitorowania danych z czujnika
+  Serial.println("lewy prawy"); 
 }
 
 void loop() {
-
-  Serial.println(pid.total_max); 
+  
+  int32_t error = readSensors ();
+    pid_motors = pid_calc(&pid,error);
+    int l_motor = vSpeed + pid_motors;
+    int r_motor = vSpeed - pid_motors;
+    Serial.print(l_motor);      
+  Serial.print(",");             
+  Serial.println(r_motor);  
    
-  delay(1000);
+  delay(20);
   
 }
 int32_t readSensors (){
@@ -135,6 +143,46 @@ void pid_init(pid_t * pid, float p, float i, float d, uint8_t f, int32_t dt_ms) 
   pid->total_max = INT32_MAX;
   pid->total_min = INT32_MIN;
   pid->dt_ms = dt_ms;
+}
+
+int32_t pid_calc(pid_t * pid, int32_t err) {
+  int32_t p, i, d, e, total;
+  //--------------P-------------//
+  e = err;
+  p = pid->p * e;
+
+  if (p > pid->p_max)
+    p = pid->p_max;
+  else if (p < pid->p_min)
+    p = pid->p_min;
+  pid->p_val = p >> pid->f;
+ //--------------I-------------//
+  i = pid->sum;
+  i +=(pid->i * e);
+  if (i > pid->i_max)
+    i = pid->i_max;
+  else if (i < pid->i_min)
+    i = pid->i_min;
+  pid->sum = i;
+  pid->i_val = i >> pid->f;
+ //--------------D-------------//
+  d = (pid->d * (e - pid->e_last));
+  if (d > pid->d_max)
+    d = pid->d_max;
+  else if (d < pid->d_min)
+    d = pid->d_min;
+  pid->d_val = d >> pid->f;
+  //Wartosc sygnalu
+  total = p + i + d;
+  if (total > pid->total_max)
+    total = pid->total_max;
+  else if (total < pid->total_min)
+    total = pid->total_min;
+  
+  pid->control = total >> pid->f;
+  pid->e_last = e;
+
+  return pid->control;
 }
 
 int32_t pid_scale(pid_t * pid, float v) {
