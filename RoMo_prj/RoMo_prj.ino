b9c3e7713d3222d3072602e5e9dc8331ca1c175d
Anna Plociennik <aniap48@gmail.com>
2020-06-04 17:38:22 +0200

    Definicja struktury regulatora PID

--- RoMo_prj/RoMo_prj.ino
+++ RoMo_prj/RoMo_prj.ino
@@ -1,5 +1,6 @@
 #define GRANICA 250 // poniżej białe, powyżej czarne
-int vSpeed = 150;        // predkosc obrotow silnika -- MAX 255
+int vSpeed = 120;        // predkosc obrotow silnika -- MAX 255
+
 //Prawy silnik
 const int motorAspeed  = 5; 
 const int motorA1      = 6;  
@@ -14,44 +15,74 @@ int left_sensor_state; //Wartosc odczytu z lewego czujnika
 
 const int right_sensor_pin =A1; // Analogicznie jak wyżej tylko tym razem dla prawego czujnika
 int right_sensor_state;
+//----------------PID-----------------//
 
-float e,last_e,e_sum,e_d,Pout,Iout,Dout,tau;
-unsigned long currentTime, previousTime;
+struct pid_t{
+  int32_t p;
+  int32_t i;
+  int32_t d;
+  int32_t p_val;
+  int32_t i_val;
+  int32_t d_val;
+  int32_t p_max;
+  int32_t i_max;
+  int32_t d_max;
+  int32_t p_min;
+  int32_t i_min;
+  int32_t d_min;
+  uint8_t f;
+  uint32_t power;
+  int32_t e_last;
+  int32_t sum;
+  int32_t total_max;
+  int32_t total_min;
+  int32_t control;
+  int32_t dt_ms;
+};
+
+struct pid_t pid;
+
+
 //Nastawy regulatora
-const float Kp = 25;  
+const float Kp = 1;  
 const float Ki = 0;
-const float Kd = 4;  
-  
+const float Kd = 0; 
+   
 void setup() {
- pinMode(motorA1, OUTPUT);
- pinMode(motorA2, OUTPUT);
- pinMode(motorB1, OUTPUT);
- pinMode(motorB2, OUTPUT);
- e = last_e = e_sum = e_d = Pout = Iout = Dout = 0;
- Serial.begin(9600);//Otworzenie portu szeregowego do monitorowania danych z czujnika 
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2, OUTPUT);
+  pinMode(motorB1, OUTPUT);
+  pinMode(motorB2, OUTPUT);
+  
+  Serial.begin(9600);//Otworzenie portu szeregowego do monitorowania danych z czujnika
 }
 
 void loop() {
- 
-/* Serial.print("Lewy czujnik: ");
-Serial.println(left_sensor_state);
-delay(500);
-Serial.print("Prawy czujnik: ");
-Serial.println(right_sensor_state);
-delay(250); */
-
-  //Ustawienie kierunku jazdy robota (Do przodu)
- // goForward();
- e = readSensors();
- if(e != 100)
- {
-    regMotors(e);
- }
- else{
-    Serial.println("STOP");
-    //delay(1000);
- }
- 
+  Serial.println(Kp);
+  
+}
+int32_t readSensors (){
+  int32_t e;
+  left_sensor_state = analogRead(left_sensor_pin);
+  right_sensor_state = analogRead(right_sensor_pin);
+  //Jedz prosto
+  if((left_sensor_state < GRANICA) && (right_sensor_state < GRANICA))
+  {
+    e = 0;
+  }//Jedz w prawo  
+  else if((left_sensor_state > GRANICA) && (right_sensor_state < GRANICA))
+  {
+    e = 2;
+  }//Jedz w lewo  
+  else if((left_sensor_state < GRANICA) && (right_sensor_state > GRANICA))
+  {
+    e = -2;
+  }//STOP    
+  else if((left_sensor_state > GRANICA) && (right_sensor_state > GRANICA))
+  {
+    e = 100;
+  } 
+  return e;
 }
 
 void goForward(){
@@ -61,72 +92,9 @@ void goForward(){
   digitalWrite(motorB2,HIGH);
 }
 
-float readSensors (){
-  left_sensor_state = analogRead(left_sensor_pin);
-  right_sensor_state = analogRead(right_sensor_pin);
-  goForward();
-  //Jedz prosto
-  if((left_sensor_state < GRANICA) && (right_sensor_state < GRANICA))
-  {
-    e = 0;
-      analogWrite (motorAspeed, vSpeed);
-      analogWrite (motorBspeed, vSpeed);
-  }//Jedz w prawo  
-  else if((left_sensor_state > GRANICA) && (right_sensor_state < GRANICA))
-  {
-    e = 2;
-      analogWrite (motorAspeed, 0);
-      analogWrite (motorBspeed, vSpeed);
-  }//Jedz w lewo  
-  else if((left_sensor_state < GRANICA) && (right_sensor_state > GRANICA))
-  {
-    e = -2;
-      analogWrite (motorAspeed, vSpeed);
-      analogWrite (motorBspeed, 0);
-  }//STOP    
-  else if((left_sensor_state > GRANICA) && (right_sensor_state > GRANICA))
-  {
-    e = 100;
-      analogWrite (motorAspeed, 0);
-      analogWrite (motorBspeed, 0);
-  } 
-  return e;
-}
-
-float computePID(float er){
-  
-  Pout = Kp*er;
-  
-  e_sum += (er);
-  Iout = Ki*e_sum;
-
-  e_d = (er - last_e);
-  Dout = Kd*e_d;
-   
-  float out = (Pout + Iout + Dout);
-  
-  last_e = er;
-  previousTime = currentTime;
-  return out;
-}
-
-void regMotors(float er){
-  float PID = computePID(er);
-
-  int l_motor = vSpeed + (int)PID;
-  int r_motor = vSpeed - (int)PID;
-  
-  l_motor = constrain(l_motor,0,250);
-  r_motor = constrain(r_motor,0,250);
-
-   analogWrite (motorAspeed,r_motor);
-   analogWrite (motorBspeed,l_motor);
-  
-  //Serial.print("Predkosc silnika lewego  = ");
-  //Serial.println(l_motor);
-  //Serial.print(" ");
-  //Serial.print("Predkosc silnika prawego  = ");
-  //Serial.println(r_motor);
-  //delay(100);
-  
+void Stop(){
+  digitalWrite (motorA1,LOW);
+  digitalWrite(motorA2,LOW);                       
+  digitalWrite (motorB1,LOW);
+  digitalWrite(motorB2,LOW);
 }
